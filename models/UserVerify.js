78158634bb7d181d3aa2d1e62ef137c0c5836732
sylvercodez <tosinotenaike3@gmail.com>
2022-10-19 06:32:47 +0100

    backend auth fix

---
+++ models/UserVerify.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserVerifySchema = new Schema({
+    userId:String,
+    uniqueString: String,
+    createdAt: Date,
+    expiresAt:Date,
+})
+
+const UserVerify = mongoose.model('UserVerify',UserVerifySchema)
+
+module.exports = UserVerify;
---
+++ server.js
@@ -0,0 +1,14 @@
+// mongodb
+require('./config/db')
+const app = require('express')();
+const port = 3001;
+
+const UserRouter = require("./api/User");
+
+// for accepting post form data
+const bodyParser = require("express").json;
+app.use(bodyParser());
+app.use('/user',UserRouter)
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`);
+})
